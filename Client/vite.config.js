16f04994d4c4bacccfec1a5db9e0618e6e8e041d
mayasofer1 <maya.9764@gmail.com>
2025-11-15 20:01:22 +0200

    fix: deploy to github pages

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'; // Import Vite configuration function
-import react from '@vitejs/plugin-react'; // React plugin for Vite
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// Export Vite config object
 export default defineConfig({
+  base: '/Cosmetics-Online-Store/',
+
+  plugins: [react()],
+
   server: {
     proxy: {
-      '/products': 'http://localhost:3000', // API route for product data (proxy to backend server)
-      '/orders': 'http://localhost:3000',   // API route for submitting orders
-    }
+      '/products': 'http://localhost:3000',
+      '/orders': 'http://localhost:3000',
+    },
   },
-  plugins: [react()], // Use React plugin (adds support for JSX, fast refresh, etc.)
 });
